da60b2fc1690784eabb0ccab3c9e9f0cec1e93b8
janaka120 <janakastill@gmail.com>
2025-04-14 15:59:08 +0800

    config vitest

---
+++ Employee-Management-System-Frontend/src/setupTests.ts
@@ -0,0 +1,15 @@
+// setupTests.ts
+import "@testing-library/jest-dom";
+
+if (!window.matchMedia) {
+  window.matchMedia = vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }));
+}
